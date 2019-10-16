884c3758bccc2ad61c228d344d5f97c12a3990ad
Rodrigo Soares <rodrigo.s.ferreira966@gmail.com>
2019-10-16 00:42:35 -0300

    Controle de motores aplicado

--- Robo.ino
+++ Robo.ino
@@ -1,6 +1,6 @@
 // =====================
 //      Variáveis 
-//======================
+// ======================
 
 //Motores
 
@@ -9,6 +9,9 @@
 #define motor_b_p1 6;		// Motor B, rotação horária
 #define motor_b_p2 7;		// Motor B, rotação antihorária
 
+// ==========================
+//      Métodos padrões 
+// ===========================
 
 void setup()
 {
@@ -17,3 +20,95 @@ void setup()
 void loop()
 {
 }
+
+// ==========================
+//      Métodos motores 
+// ===========================
+
+// Referente ao robô
+
+void ir_pra_frente(){
+	frente_motor_a();
+	frente_motor_b();
+}
+
+void ir_pra_tras(){
+	re_motor_a();
+	re_motor_b();
+}
+
+void frear(){
+	freio_motor_a();
+	freio_motor_b();
+}
+
+void virar_direita(){
+	frente_motor_a();
+	re_motor_b();
+}
+
+void virar_esquerda(){
+	re_motor_a();
+	frente_motor_b();
+}
+
+
+// Referente ao motor A 
+
+void frente_motor_a(){
+	digitalWrite(motor_a_p1, HIGH);
+	digitalWrite(motor_a_p2, LOW);
+}
+
+void freio_motor_a(){
+	digitalWrite(motor_a_p1, HIGH);
+	digitalWrite(motor_a_p2, HIGH);
+}
+
+void re_motor_a(){
+	digitalWrite(motor_a_p1, LOW);
+	digitalWrite(motor_a_p2, HIGH);
+}
+
+// Referente ao motor B 
+
+void frente_motor_b(){
+	digitalWrite(motor_b_p1, HIGH);
+	digitalWrite(motor_b_p2, LOW);
+}
+
+void freio_motor_b(){
+	digitalWrite(motor_b_p1, HIGH);
+	digitalWrite(motor_b_p2, HIGH);
+}
+
+void re_motor_b(){
+	digitalWrite(motor_b_p1, LOW);
+	digitalWrite(motor_b_p2, HIGH);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
