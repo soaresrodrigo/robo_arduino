ee7d6511d631f4d8fb76b7f09c380cbb0eb9c6b2
Rodrigo Soares <rodrigo.s.ferreira966@gmail.com>
2019-10-16 00:32:09 -0300

    Adicionando um padrão para se trabalhar

--- Robo.ino
+++ Robo.ino
@@ -3,9 +3,17 @@
 //======================
 
 //Motores
-int motor_a_p1 = 4;
-int motor_a_p2 = 5;
-int motor_b_p1 = 6;
-int motor_b_p2 = 7;
+
+#define motor_a_p1 4;		// Motor A, rotação horária
+#define motor_a_p2 5;		// Motor A, rotação antihorária
+#define motor_b_p1 6;		// Motor B, rotação horária
+#define motor_b_p2 7;		// Motor B, rotação antihorária
 
 
+void setup()
+{
+}
+
+void loop()
+{
+}
