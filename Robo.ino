59e255dfd7b6a8aafd34d35ae0f2db94fc658f86
Rodrigo Soares <rodrigo.s.ferreira966@gmail.com>
2019-10-16 00:26:55 -0300

    Padronizando as variáveis

--- Robo.ino
+++ Robo.ino
@@ -1,1 +1,11 @@
-Tentando modificar essa linha também
+// =====================
+//      Variáveis 
+//======================
+
+//Motores
+int motor_a_p1 = 4;
+int motor_a_p2 = 5;
+int motor_b_p1 = 6;
+int motor_b_p2 = 7;
+
+
