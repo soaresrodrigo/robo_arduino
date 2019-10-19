bc2f2befa184adb658a6283b220784509119e4f8
Rodrigo Soares <rodrigo.s.ferreira96@gmail.com>
2019-10-18 21:16:43 -0300

    Versão testada
    Ativando tudo

--- Robo.ino
+++ Robo.ino
@@ -4,29 +4,25 @@
 // ======================
 
 // Servo
-#define SERVO 7 
+#define SERVO 13 
 Servo servo; // Instancia o servo
 
 //Motores
 
-#define motor_a_p1 4    // Motor A, rotação horária
+#define motor_a_p1 2    // Motor A, rotação horária
 #define motor_a_p2 3   // Motor A, rotação antihorária
 
-#define motor_b_p1 2   // Motor B, rotação horária
+#define motor_b_p1 4   // Motor B, rotação horária
 #define motor_b_p2 5   // Motor B, rotação antihorária
 
 //Sensores
 
-#define sensor_de_linha_frente 4   // Sensor de linha da frente
-#define sensor_de_linha_atras 4    // Sensor de linha de trás
+#define sensor_de_linha_frente 9   // Sensor de linha da frente
+#define sensor_de_linha_atras 8    // Sensor de linha de trás
 
-#define sensor_de_infra_frente 8     // Sensor infravermelho frontal
-#define sensor_de_infra_esquerdo 9   // Sensor infravermelho esquerdo
+#define sensor_de_infra_frente 6     // Sensor infravermelho frontal
+#define sensor_de_infra_esquerdo 10   // Sensor infravermelho esquerdo
 #define sensor_de_infra_direito 11    // Sensor infravermelho direito
-
-#define pino_vermelho 9
-#define pino_azul 10
-#define pino_verde 11
 
 
 // ==========================
@@ -36,16 +32,15 @@ Servo servo; // Instancia o servo
 void setup(){
   servo.attach(SERVO);
   servo.write(0);
-//  delay(5000);
+  delay(5000);
   servo.write(90);
   pinosMotores();
   pinosSensores();  
 }
 
 void loop(){  
-//afastaDaLinha();
-//ataque();
-
+afastaDaLinha();
+ataque();
 }
 
 // ==========================
@@ -63,36 +58,26 @@ void pinosSensores(){
 void afastaDaLinha(){
   bool sensor_frente = digitalRead(sensor_de_linha_frente);
   bool sensor_atras = digitalRead(sensor_de_linha_atras);
-  
+
   if(!sensor_frente){
-    frear();
-    virar_direita();
-  }
-  
-  if(!sensor_atras){
+    ir_pra_tras();
+  }else if(!sensor_atras){
     ir_pra_frente();
-  }
+  }else{
+    frear(); 
+   }
 }
 
 void ataque(){
   bool infra_frente = digitalRead(sensor_de_infra_frente);  
   bool infra_esquerdo = digitalRead(sensor_de_infra_esquerdo);    
   bool infra_direito = digitalRead(sensor_de_infra_direito);
-  
-  if(!infra_frente){
-    ir_pra_frente();
-    ir_pra_tras();
-  }else{
-    frear();
-    }
-  
- if(!infra_esquerdo){
-   virar_esquerda();
-  }
-  
-  if(!infra_direito){
-    virar_direita();
-  }
+
+  if(!infra_frente) ir_pra_frente();
+  else if(!infra_esquerdo) virar_direita();
+  else if(!infra_direito) virar_esquerda();
+  else frear(); 
+
 }
 
 // ==========================
@@ -140,12 +125,10 @@ void frente_motor_a(){
   digitalWrite(motor_a_p1, HIGH);
   digitalWrite(motor_a_p2, LOW);
 }
-
 void freio_motor_a(){
   digitalWrite(motor_a_p1, HIGH);
   digitalWrite(motor_a_p2, HIGH);
 }
-
 void re_motor_a(){
   digitalWrite(motor_a_p1, LOW);
   digitalWrite(motor_a_p2, HIGH);
@@ -157,18 +140,11 @@ void frente_motor_b(){
   digitalWrite(motor_b_p1, HIGH);
   digitalWrite(motor_b_p2, LOW);
 }
-
 void freio_motor_b(){
   digitalWrite(motor_b_p1, HIGH);
   digitalWrite(motor_b_p2, HIGH);
 }
-
 void re_motor_b(){
   digitalWrite(motor_b_p1, LOW);
   digitalWrite(motor_b_p2, HIGH);
 }
-
-
-
-
-  
